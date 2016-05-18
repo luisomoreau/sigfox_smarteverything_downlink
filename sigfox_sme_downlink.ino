839325af53b77047fdfa02c0a8e41f1049f4485f
luisomoreau <luis.omoreau@gmail.com>
2016-05-18 11:24:17 +0200

    added uplink on button one and downlink on button two

--- sigfox_sme_downlink.ino
+++ sigfox_sme_downlink.ino
@@ -1,7 +1,7 @@
 /*
  *  Subject: This project demonstrate how to realise a downlink using the smarteverything board.
  *           Depending on the ACK value, the led will change its color when it is received by the SmartEveryThing board.
- *           The request will be asked by the card every 10 minutes or when the button is pushed.
+ *           The request will be asked by the card every 10 minutes or when the button is pressed.
  *  
  *  Created by: Louis Moreau
  *  Date: 17 May 2016
@@ -21,13 +21,127 @@
 
 
 void setup() {
+  //LED & User Button are already initialized by the SME core
+
+  // Init UART devices
   SerialUSB.begin(115200);
   SigFox.begin(19200);
-  
+  delay(100);
+  //setConfigurationMode();
+  delay(1000);
+  getID();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //Here we send simple message using Sigfox (uplink) when the button one is pressed
+  if (isButtonOnePressed()) {
+    sendSimpleSigfoxMessage("CAFE");
+    }
 
+  //Here we send a message and we wait to receive the ACK using Sigfox (downlink) when the button two is pressed
+  if (isButtonTwoPressed()) {
+     sendAndReceiveSigfoxMessage();
+   }
+
+}
+
+void sendSimpleSigfoxMessage(String frame){
+  resetLEDs();
+  String status = "";
+  char output;
+  SerialUSB.print("AT$SF=");
+  SerialUSB.println(frame);
+  SigFox.print("AT$SF=");
+  SigFox.print(frame);
+  SigFox.print("\r");
+  while (!SigFox.available()){
+     blink();
+  }
+  while(SigFox.available()){
+    output = (char)SigFox.read();
+    status += output;
+    delay(10);
+  }
+  if(DEBUG){
+    SerialUSB.print("Status \t");
+    SerialUSB.println(status);
+  }
+  
+ if (status == "OK\r"){
+  //Success :)
+  ledGreenLight(HIGH);
+  }
+ else{
+  ledRedLight(HIGH);
+  }
+}
+
+void sendAndReceiveSigfoxMessage(){
+  resetLEDs();
+  String status = "";
+  char output;
+  
+  SerialUSB.println("AT$SF=6e69636f6c7363,1\r");
+
+  delay(1000);
+  SigFox.print("AT$SF=6e69636f6c7363,1\r");
+  while (!SigFox.available()){
+     blink();
+  }
+  while(SigFox.available()){
+    output = (char)SigFox.read();
+    status += output;
+    delay(10);
+  }
+  SerialUSB.println("Status \t");
+  SerialUSB.println(status);
+   if (status == "OK\r"){
+    //Success :)
+    ledGreenLight(HIGH);
+  }
+  else{
+    ledRedLight(HIGH);
+  }
+  
+  
+}
+
+//This function gives you the device ID
+String getID(){
+  String id = "";
+  char output;
+  
+  SigFox.print("AT$ID\r");
+  while (!SigFox.available()){
+     blink();
+  }
+  
+  while(SigFox.available()){
+    output = (char)SigFox.read();
+    id += output;
+    delay(10);
+  }
+  
+  if(DEBUG){
+    SerialUSB.println("Sigfox Device ID: ");
+    SerialUSB.println(id);
+  }
+  
+  return id;
+}
+
+
+void blink(){
+  ledBlueLight(HIGH);
+  delay(100);
+  ledBlueLight(LOW);
+  delay(200);
+}
+
+void resetLEDs(){
+  ledRedLight(LOW);
+  ledGreenLight(LOW);
+  ledBlueLight(LOW);
 }
 
